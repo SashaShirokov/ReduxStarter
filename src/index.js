834c1bac63cf9f2cc471da9578fbd0a8fe210f02
Sasha <lyshirokov613@gmail.com>
2016-12-24 22:04:11 +0300

    Added some style

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 
 const API_KEY = "AIzaSyDYZ0KGbplmjdd96JN-SRgywqhI6UQbZ9o";
 
@@ -12,18 +13,26 @@ class  App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [] ,
+      selectedVideo: null
+    };
 
-    YTSearch({key: API_KEY, term: "react.js"}, (videos) => {
-      this.setState({ videos });
+    YTSearch({key: API_KEY, term: "surfboards"}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
   render() {
     return (
       <div>
-        <h1>Hello!!!</h1>
         <SearchBar/>
-        <VideoList videos={this.state.videos}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos}/>
       </div>
     );
   };
